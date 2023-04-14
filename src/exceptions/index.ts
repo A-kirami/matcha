fbb62a4eb8035025c17a36484b359000bfafa2aa
Akirami <66513481+A-kirami@users.noreply.github.com>
2023-04-15 00:46:04 +0800

    🔥 移除 exceptions

--- src/exceptions/index.ts
+++
@@ -1,9 +0,0 @@
-export class AdapterError extends Error {}
-
-export class DriverError extends Error {}
-
-export class BotError extends Error {}
-
-export class UnknownEventError extends AdapterError {}
-
-export class UnsupportedMessageError extends AdapterError {}
