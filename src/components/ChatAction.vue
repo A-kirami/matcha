74c6a0d845b9136300cfc98de384c551fd4c255e
Akirami <66513481+A-kirami@users.noreply.github.com>
2024-02-28 19:49:08 +0800

    🚨 修复 unocss 排序警告

--- src/components/ChatAction.vue
+++ src/components/ChatAction.vue
@@ -6,5 +6,5 @@ defineProps<{
 </script>
 
 <template>
-  <section class="w-64 flex-shrink-0 border-l lt-lg:hidden lt-xl:w-48" dark="border-dark-400"></section>
+  <section class="w-64 flex-shrink-0 border-l lt-xl:w-48 lt-lg:hidden" dark="border-dark-400"></section>
 </template>
