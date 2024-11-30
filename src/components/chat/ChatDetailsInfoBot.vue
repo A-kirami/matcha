88bdf33daf114ae21982d82ba987de3ae0ede140
Akirami <66513481+A-kirami@users.noreply.github.com>
2024-11-30 23:14:35 +0800

    ✨ 添加快速刷新连接按钮

--- src/components/chat/ChatDetailsInfoBot.vue
+++ src/components/chat/ChatDetailsInfoBot.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+const state = useStateStore()
+</script>
+
 <template>
-  <div></div>
+  <div class="flex flex-row cursor-pointer items-center gap-2" @click="state.refreshBot()">
+    <span class="relative size-2 flex">
+      <span
+        class="absolute h-full w-full inline-flex rounded-full opacity-75"
+        :class="{ 'bg-emerald-300 animate-ping': state.isConnected }"
+      ></span>
+      <span
+        class="relative size-2 inline-flex rounded-full bg-emerald-400"
+        :class="[state.isConnected ? 'bg-emerald-400' : 'bg-rose-400']"
+      ></span>
+    </span>
+    <span class="text-xs text-muted-foreground">{{ state.isConnected ? '已连接' : '未连接' }}</span>
+  </div>
 </template>
