6f699d039f4ee8086a468cdd64c565bd74a169e9
Akirami <66513481+A-kirami@users.noreply.github.com>
2024-12-02 02:59:12 +0800

    🐛 修复子窗口没有应用透明度的问题 (#208)

--- src/layouts/native.vue
+++ src/layouts/native.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="grid grid-rows-[auto_minmax(0,1fr)] h-screen">
-    <WindowTitlebar class="h-7" />
-    <RouterView />
+  <div
+    class="grid grid-rows-[auto_minmax(0,1fr)] h-screen"
+  >
+    <WindowTitlebar
+      class="h-7" :class="[
+        focused ? 'bg-opacity-40 dark:bg-opacity-60' : 'bg-opacity-70 dark:bg-opacity-90'
+      ]"
+    />
+    <RouterView
+      :class="[
+        focused ? 'bg-opacity-40 dark:bg-opacity-60' : 'bg-opacity-70 dark:bg-opacity-90'
+      ]"
+    />
   </div>
 </template>
